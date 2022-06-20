10679f98318fb5aaacd89c6cd029e5ad8c5a64d5
Thatslimdev <emekajerry514@gmail.com>
2022-06-20 23:37:24 +0100

    intro to JS

---
+++ index.js
@@ -0,0 +1,9 @@
+const Person = {
+	Name : 'ENUSONUM EMEKA JERRY',
+	Height : '180cm',
+	Country : 'Nigeria',
+
+
+}
+
+document.write("<h2>Name:  " + Person.Name + "<br><h3> Height:  " + Person.Height + "<br><h4>Country: " + Person.Country)
